9637c911c471df7bf0686684b962ba2e4215dca5
phuc220204 <phuc220204@gmail.com>
2026-08-16 03:32:12 +0700

    feat(web): compose responsive public homepage

--- apps/web/src/features/marketplace/components/home/HomePage.tsx
+++ apps/web/src/features/marketplace/components/home/HomePage.tsx
@@ -1,3 +1,23 @@
+import { FeaturedRoomsSection } from '@/features/marketplace/components/home/FeaturedRoomsSection';
+import { HeroSection } from '@/features/marketplace/components/home/HeroSection';
+import { LandlordCTA } from '@/features/marketplace/components/home/LandlordCTA';
+import { MarketplaceSections } from '@/features/marketplace/components/home/MarketplaceSections';
+import { PostingCTASection } from '@/features/marketplace/components/home/PostingCTASection';
+import { SiteFooter } from '@/features/marketplace/components/home/SiteFooter';
+import { WhyUsSection } from '@/features/marketplace/components/home/WhyUsSection';
+
 export function HomePage() {
-  return null;
+  return (
+    <>
+      <main className="flex-1 bg-canvas">
+        <HeroSection />
+        <FeaturedRoomsSection />
+        <MarketplaceSections />
+        <WhyUsSection />
+        <PostingCTASection />
+        <LandlordCTA />
+      </main>
+      <SiteFooter />
+    </>
+  );
 }
